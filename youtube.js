aec52a99104f42184fbb9d8536b8f3a5898f9ca7
sindystar <hasungah@gmail.com>
2023-05-06 22:24:51 +0900

    오류 확인 수정

--- youtube.js
+++ youtube.js
@@ -1,24 +1,24 @@
-// key =  AIzaSyDq1ThuKd63CGMc178rIvnscNriIww6L4A
-// play list = PLYOPkdUKSFgVaH3wgkzGpMuGb29dDZl6j
+// key =  AIzaSyB-6xV4QXok9PwwPhxxeP3c4JzN_KmmKKY
+// play list =  PLfYNuKPjo-c-_IvgCMUV4AhGDTrFFI8Qu
 
 const main = document.querySelector("main");
-const key = "AIzaSyDq1ThuKd63CGMc178rIvnscNriIww6L4A";
-const playListId = "PLYOPkdUKSFgVaH3wgkzGpMuGb29dDZl6j";
+const key = "AIzaSyB-6xV4QXok9PwwPhxxeP3c4JzN_KmmKKY";
+const playListId = "PLfYNuKPjo-c-_IvgCMUV4AhGDTrFFI8Qu";
 const num = 6;
-const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playListId}&maxResults=${num}`;
+const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playListId}&maxReseults=${num}`;
 
 /*
-json 객체 
+json 객체
 {
   "name" : "식빵",
   "family" : "웰시코기",
 }
 
-json 배열
-"배열이름" : [
-  {"name" : "식빵",  "family" : "웰시코기",},
-  {"name" : "식빵",  "family" : "웰시코기",},
-  {"name" : "식빵",  "family" : "웰시코기",},
+json 배열 
+"배열이름"  : [
+  {"name" : "식빵", "family" : "웰시코기",},
+  {"name" : "식빵", "family" : "웰시코기",},
+  {"name" : "식빵", "family" : "웰시코기",},  
 ]
 
 */
@@ -27,10 +27,10 @@ fetch(url)
   .then((data) => {
     // console.log(data);
     return data.json();
-
+    
   })
   .then((json) => {
-    //console.log(json);
+    // console.log(jason);
     let items = json.items;
     console.log(items);
 
