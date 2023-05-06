a0d1a1de1e227c865fd8800978172999be4c06be
sindystar <hasungah@gmail.com>
2023-05-06 22:16:52 +0900

    클론 JS / 오류 확인용

--- youtube.js
+++ youtube.js
@@ -1,37 +1,37 @@
-// key =  AIzaSyCuAyJp7mF1SltU9KSBUP0oc_OXszMzlk0
-// play list =  PLfYNuKPjo-c-O_aCqmoSgbH4U48JD9SGsplay
+// key =  AIzaSyDq1ThuKd63CGMc178rIvnscNriIww6L4A
+// play list = PLYOPkdUKSFgVaH3wgkzGpMuGb29dDZl6j
 
 const main = document.querySelector("main");
-const key = "AIzaSyCuAyJp7mF1SltU9KSBUP0oc_OXszMzlk0";
-const playListId = "PLfYNuKPjo-c-O_aCqmoSgbH4U48JD9SGsplay";
+const key = "AIzaSyDq1ThuKd63CGMc178rIvnscNriIww6L4A";
+const playListId = "PLYOPkdUKSFgVaH3wgkzGpMuGb29dDZl6j";
 const num = 6;
-const url =`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playListId}&maxReseult=${num}`;
+const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playListId}&maxResults=${num}`;
 
 /*
-jason 객체
+json 객체 
 {
   "name" : "식빵",
   "family" : "웰시코기",
 }
 
-jason 배열 
-"배열이름"  : [
-  {"name" : "식빵", "family" : "웰시코기",},
-  {"name" : "식빵", "family" : "웰시코기",},
-  {"name" : "식빵", "family" : "웰시코기",},  
+json 배열
+"배열이름" : [
+  {"name" : "식빵",  "family" : "웰시코기",},
+  {"name" : "식빵",  "family" : "웰시코기",},
+  {"name" : "식빵",  "family" : "웰시코기",},
 ]
 
 */
 
 fetch(url)
-  .then((data)=> {
-    // consol.log(data);
-    return data.jason();
-    
+  .then((data) => {
+    // console.log(data);
+    return data.json();
+
   })
-  .then((jason) => {
-    // console.log(jason);
-    let items = jason.items;
+  .then((json) => {
+    //console.log(json);
+    let items = json.items;
     console.log(items);
 
     let result = "";
@@ -47,4 +47,5 @@ fetch(url)
       `;
     });
     main.innerHTML = result;
+
   })
